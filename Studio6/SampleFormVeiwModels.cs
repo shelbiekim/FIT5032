61f3a86dd10a507860ad1380e1da58a51de4ae28
Miji Kim <mkim0021@student.monash.edu>
2020-09-09 09:54:15 +1000

    Add code relevant to FormOneViewModel class

---
+++ Studio6/SampleFormVeiwModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FIT5032_Week6.Models
+{
+    public class SampleFormVeiwModels
+    {
+    }
+
+    public class FormOneViewModel
+    { 
+        [Required] //required field, validation
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
