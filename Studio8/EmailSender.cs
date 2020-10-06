365f4f6ec358ea4df3a1473983cacb86e71e7113
Miji Kim <mkim0021@student.monash.edu>
2020-10-07 09:51:39 +1100

    Add EmailSender.cs file

---
+++ Studio8/EmailSender.cs
@@ -0,0 +1,28 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FIT5032_Week08A.Utils
+{
+    public class EmailSender
+    {
+        // Please use your API KEY here.
+        private const String API_KEY = "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";
+
+        public void Send(String toEmail, String subject, String contents)
+        {
+            var client = new SendGridClient(API_KEY);
+            var from = new EmailAddress("shelbkim@gmail.com", "FIT5032 Example Email User");
+            var to = new EmailAddress("shelbkim@gmail.com", "");
+            var plainTextContent = contents;
+            var htmlContent = "<p>" + contents + "</p>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = client.SendEmailAsync(msg);
+        }
+
+    }
+}
